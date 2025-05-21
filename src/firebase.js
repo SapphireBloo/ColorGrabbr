9ead819503586a779ded941bb4a62b9be7fd7f23
Dustin-Work <Dustinboydxx@gmail.com>
2025-05-21 10:35:17 -0500

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOtvAsl8n1yYYI474_iD6f2bC5okohVlQ",
-  authDomain: "color-picker-fe592.firebaseapp.com",
-  projectId: "color-picker-fe592",
-  storageBucket: "color-picker-fe592.appspot.com", // ✅ Fixed typo
-  messagingSenderId: "364300221921",
-  appId: "1:364300221921:web:9d3c6dcf9edb41d6f7a6a0",
-  measurementId: "G-B82L74R61E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
